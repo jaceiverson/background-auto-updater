1fa9205d122631621924841b83f06278f8b90c45
Jace Iverson <iverson.jace@gmail.com>
2024-01-26 12:44:26 -0700

    :recycle: Updated to find images not on 5minutes
    There was a change in the images for Snowbasin, so I alterted the script to check mintue values that are multiples of 5. So we can now find images that are 01, 02, 03, 04 etc.

--- snowbasin_image.py
+++ snowbasin_image.py
@@ -5,6 +5,9 @@ import logging
 import glob
 import requests
 from rich.logging import RichHandler
+from rich.traceback import install
+install(show_locals=True)
+
 
 logging.basicConfig(
     level="NOTSET",
@@ -15,7 +18,7 @@ logger = logging.getLogger("rich")
 
 
 class SnowbasinImage:
-    def __init__(self, background_directory: str, store_previous_images:bool = True) -> None:
+    def __init__(self, background_directory: str, store_previous_images:bool = True,image_size:str="1080") -> None:
         """
         initialize the class
         background_directory: the directory where the images will be saved. This is an absolute path.
@@ -24,33 +27,103 @@ class SnowbasinImage:
         self.background_file_path = background_directory
         self.store_previous_images = store_previous_images
         self.base_url = "https://storage.googleapis.com/prism-cam-00054"
+        self.image_size = image_size
 
-    def get_image(self, url: str, file_path: str) -> bool:
+    def set_image_size(self,image_size:str) -> None:
+        """
+        set the image size
+        """
+        self.image_size = image_size
+
+    def make_url_string(self,date:dt.datetime) -> str:
+        """
+        make the url string
+        """
+        return f"{self.base_url}/{date.strftime('%Y')}/{date.strftime('%m')}/{date.strftime('%d')}/{date.strftime('%H')}-{date.strftime('%M')}/{self.image_size}.jpg"
+    
+    def make_file_path_string(self,date:dt.datetime,full_path:bool = True) -> str:
+        """
+        make the file path string
+        """
+        if full_path:
+            return f"{self.background_file_path}{date.strftime('%Y')}-{date.strftime('%m')}-{date.strftime('%d')}-{date.strftime('%H')}-{date.strftime('%M')}.jpg"
+        return f"{date.strftime('%Y')}-{date.strftime('%m')}-{date.strftime('%d')}-{date.strftime('%H')}-{date.strftime('%M')}.jpg"
+    
+    @staticmethod
+    def make_date_from_file_string(file_name:str) -> dt.datetime:
+        """
+        make a date from the file name
+        """
+        return dt.datetime.strptime(file_name,"%Y-%m-%d-%H-%M.jpg")
+    
+    def check_for_non_round_image_times(self,image_time_to_pull:dt.date,request_limit:int=0) -> requests.models.Response:
+        """
+        check for non round image times
+        if we don't find an image it will look for the previous 4 minute intervals before it
+        for example: 
+        if we are looking for 
+        2021-09-10 10:15:00 
+        
+        and we don't find it
+        it will look for 
+
+        2021-09-10 10:14:00
+        then 2021-09-10 10:13:00
+        then 2021-09-10 10:12:00
+        then 2021-09-10 10:11:00
+
+        if we don't find any of those, it will just return the resp object which will be a 404
+        log the result, and don't save a picture
+        """
+        url = self.make_url_string(image_time_to_pull)
+        logger.info(f"[blue]Checking for image at: {url}")
+        resp = requests.get(url)
+        if resp.ok:
+            logger.info(f"[green]Image found at: {url}")
+            return resp,image_time_to_pull
+        else:
+            logger.info(f"[yellow]Image not found at [/]{url}")
+            if request_limit == 5:
+                logger.warn(f"[red]No image found in 5 tries")
+                return resp
+            request_limit += 1
+            image_time_to_pull = image_time_to_pull - dt.timedelta(minutes=request_limit)
+            return self.check_for_non_round_image_times(image_time_to_pull,request_limit)
+
+
+    def get_image(self, image_time_to_pull:dt.date) -> bool:
         """
         scrape Snowbasin image and save to my computer
         return True if successful, False if not
         """
-        resp = requests.get(url)
+        resp,image_time = self.check_for_non_round_image_times(image_time_to_pull)
+        # if we get a good response, we will save the image
         if resp.ok:
             img_data = resp.content
-            with open(file_path, "wb") as handler:
+            file_path = self.make_file_path_string(image_time)
+            with open(os.path.expanduser(file_path), "wb") as handler:
                 handler.write(img_data)
-            logger.info(f"Image downloaded and saved to [yellow]{file_path}")
+            logger.info(f"[green]Image downloaded and saved to: {file_path}")
             return True
         else:
             return False
 
     @staticmethod
-    def find_last_image_time() -> tuple:
+    def find_next_image_time(current_background:dt.datetime) -> dt.datetime:
         """
-        return the last good image time
-        formatted as a tuple
-        (year, month, day, hour, minute)
+        return the next good image time as datetime to look for
         """
         right_now = dt.datetime.now()
+        if right_now - current_background < dt.timedelta(minutes=5):
+            # if at least 6 minutes haven't passed, return the current background
+            # this will skip looking for new images
+            return current_background
+        
         if right_now.hour >= 8 and right_now.hour <= 18:
+            # between 8am and 6pm we will look for images every 5 minutes
             right_now = right_now.replace(minute=right_now.minute // 5 * 5)
         else:
+            # outside of 8am and 6pm we will look for images every 15 minutes
             if right_now.minute < 5:
                 right_now = right_now.replace(minute=50)
                 right_now = right_now - dt.timedelta(hours=1)
@@ -62,30 +135,35 @@ class SnowbasinImage:
                 right_now = right_now.replace(minute=35)
             elif right_now.minute < 60:
                 right_now = right_now.replace(minute=50)
-        return (
-            right_now.strftime("%Y"),
-            right_now.strftime("%m"),
-            right_now.strftime("%d"),
-            right_now.strftime("%H"),
-            right_now.strftime("%M"),
-        )
-
-    def get_files_in_directory(self):
+        return right_now
+    
+    @staticmethod
+    def parse_date_values(date:dt.datetime) -> tuple[str]:
         """
-        returns all files in the backgrounds folder
+        parse the date values into string values return as tuple
+        """
+        return date.strftime('%Y'),date.strftime('%m'),date.strftime('%d'),date.strftime('%H'),date.strftime('%M')
+
+    def get_files_in_directory(self) -> str:
+        """
+        returns the current file in the backgrounds folder
+        if there is more than 1 file, it will raise an error
         """
         try:
             # Use glob to get a list of files in the directory matching the pattern
-            return glob.glob(
+            files = glob.glob(
                 os.path.join(os.path.expanduser(self.background_file_path), "*.jpg")
             )
+            if len(files)>1:
+                raise OSError("More than one file in the directory")
+            return files[0]
             # Print the list of files
         except OSError as e:
             logger.error(f"Error: {e}")
 
     def move_last_image(self, old_file_path: str):
         """
-        move the older file to the archive folder
+        move the older image file to the archive folder
         """
         old_file_name = os.path.basename(old_file_path)
         new_backgrounds_path = os.path.expanduser(
@@ -99,45 +177,42 @@ class SnowbasinImage:
         # move the file
         os.rename(old_file_path, new_file_path)
 
-    def delete_last_image(self,old_file_path: str):
+    @staticmethod
+    def delete_file(old_file_path: str):
         """
-        delete the older file
+        delete file at the given path
         """
         os.remove(old_file_path)
 
     def process(self):
-        # check the date time
-        last_image_time = self.find_last_image_time()
-        # unpack the date time
-        year, month, day, hour, minute = last_image_time
-        # set the file path
-        desktop_path = os.path.expanduser(self.background_file_path)
-        file_name = f"{year}-{month}-{day}-{hour}-{minute}.jpg"
+        """
+        main process for looking for the right time
+        pulling image
+        and moving/deleting the old image
+        """
         # pull in the current contents of the directory
-        current_files = self.get_files_in_directory()
-        # check if the file exists there already
-        if file_name in current_files[0] or len(current_files) > 1:
-            logger.info(f"Image already exists: [yellow]{file_name}")
+        current_background_file = self.get_files_in_directory()
+        logger.info(f"[blue]Current background image: {current_background_file}")
+        # convert to a datetime object 
+        current_background_image_date = self.make_date_from_file_string(current_background_file.split("/")[-1])
+        # check the date time
+        next_image_to_pull = self.find_next_image_time(current_background_image_date)
+        # check if the image already exists, we will log and exit the function
+        if next_image_to_pull == current_background_image_date:
+            logger.info(f"[yellow]Not enough time has passed (5 min). Image already exists: {current_background_file}")
             return True
-        # we will pull a new file
-        old_file_name = current_files[0]
-        # set the image size
-        image_size = "1080"
-        # set the base url
-        base_url = f"{self.base_url}/{year}/{month}/{day}/{hour}-{minute}/{image_size}.jpg"
-        logger.info(f"Checking for image at [yellow]{base_url}")
         # get the image
-        if self.get_image(base_url, os.path.join(desktop_path, file_name)):
+        if self.get_image(next_image_to_pull):
+            # depending on the value of store_previous_images, we will either move or delete the file
             if self.store_previous_images:
                 # move the file
-                self.move_last_image(old_file_name)
-                logger.info(f"Moved {os.path.basename(old_file_name)} to archive folder.")
+                self.move_last_image(current_background_file)
+                logger.info(f"[yellow]Moved {os.path.basename(current_background_file)} to archive folder.")
             else:
                 # delete the file
-                self.delete_last_image(old_file_name)
-                logger.info(f"Deleted {os.path.basename(old_file_name)}")
+                self.delete_file(current_background_file)
+                logger.info(f"[yellow]Deleted {os.path.basename(current_background_file)}")
         else:
-            logger.info(f"[red]Image not found at [yellow]{base_url}")
             return False
 
 
