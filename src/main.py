9d48acdfbdd9f1596114d9788b6f366bcd294e69
Jace Iverson <iverson.jace@gmail.com>
2024-01-27 13:48:09 -0700

    :sparkles: Added flag for folder path

--- src/main.py
+++ src/main.py
@@ -5,20 +5,20 @@ import time
 from snowbasin_image import SnowbasinImage, logger
 
 
-def once():
+def once(folder_path:str):
     """
     Run the script once and exit
     """
-    s = SnowbasinImage("~/Desktop/backgrounds/")
+    s = SnowbasinImage(folder_path)
     s.process()
 
 
-def constant(minute_interval: int = 5):
+def constant(folder_path:str, minute_interval: int = 5):
     """
     Run the script constantly and check for new images every minute_interval minutes
     """
     try:
-        s = SnowbasinImage("~/Desktop/backgrounds/")
+        s = SnowbasinImage(folder_path)
         while True:
             s.process()
             time.sleep(60 * minute_interval)
@@ -46,12 +46,19 @@ def main():
         default=5,
         help="how often to check for new images in minutes",
     )
+    parser.add_argument(
+        "-f",
+        "--folder-path",
+        type=str,
+        default="~/Desktop/backgrounds",
+        help="folder path to save images to",
+    )
     args = parser.parse_args()
     logger.info(f"Running with args: {args}")
     if args.constant:
-        constant(args.minute_interval)
+        constant(args.folder_path,args.minute_interval)
     else:
-        once()
+        once(args.folder_path)
 
 
 if __name__ == "__main__":
